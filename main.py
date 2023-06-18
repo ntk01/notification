816088aa6415624a57f4621cf8fac49170bedd6b
ntk01 <n.taku.law@gmail.com>
2023-06-18 19:31:48 +0900

    func

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
 jst = timezone(timedelta(hours=+9), 'JST')
 
-ja_req = requests.get('https://www.worldometers.info/world-population/japan-population/')
-us_req = requests.get('https://www.worldometers.info/world-population/us-population/')
-ch_req = requests.get('https://www.worldometers.info/world-population/china-population/')
-ja_soup = BeautifulSoup(ja_req.content, features='html.parser')
-us_soup = BeautifulSoup(us_req.content, features='html.parser')
-ch_soup = BeautifulSoup(ch_req.content, features='html.parser')
+ja_url = 'https://www.worldometers.info/world-population/japan-population/'
+us_url = 'https://www.worldometers.info/world-population/us-population/'
+ch_url = 'https://www.worldometers.info/world-population/china-population/'
 
-ja_target = ja_soup.find('div', attrs={'class': 'col-md-8 country-pop-description'}).find_all_next('strong')[1]
-us_target = us_soup.find('div', attrs={'class': 'col-md-8 country-pop-description'}).find_all_next('strong')[1]
-ch_target = ch_soup.find('div', attrs={'class': 'col-md-8 country-pop-description'}).find_all_next('strong')[1]
+def getTarget(url):
+  req = requests.get(url)
+  soup = BeautifulSoup(req.content, features='html.parser')
+  return soup.find('div', attrs={'class': 'col-md-8 country-pop-description'}).find_all_next('strong')[1]
+
+ja_target = getTarget(ja_url)
+us_target = getTarget(us_url)
+ch_target = getTarget(ch_url)
 
 text = f'【人口情報】\n日時: {datetime.now(jst).ctime()}\n\n日本: {ja_target.text}\nアメリカ: {us_target.text}\n中国: {ch_target.text}\n\n取得元: https://www.worldometers.info/'
 
