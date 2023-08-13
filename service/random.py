c087bcca28c6326c66d9c97bd6296e49e9742b8f
ntk01 <n.taku.law@gmail.com>
2023-08-13 19:16:21 +0900

    log

--- service/random.py
+++ service/random.py
@@ -11,20 +11,19 @@ db = firestore.client()
 jst = timezone(timedelta(hours=+9), 'JST')
 date = datetime.now(jst)
 
-n = random.choice(rand() + getAll())
-
-def rand():
+def rangeList():
   return [*range(1, 101, 1)]
 
 def getAll() -> list:
   docs = db.collection("numbers").stream()
   return list(map(lambda doc: doc.to_dict().get('number'), docs))
 
-n = random.choice(rand() + getAll())
+n = random.choice(rangeList() + getAll())
 
 def setOne():
   db.collection("numbers").add(vars(number.Number(number=n)))
 
 def getText():
   print(getAll())
+  print(n)
   return f'{date.ctime()}\n\n1から100までの乱数を表示。\n表示した数字はDBにセット。\n1から100までの数列にDBから取得したリストを結合して乱数を表示。\n回を重ねると偏りが見られるかの実験。\n\n今選択されたのは...{n}'
